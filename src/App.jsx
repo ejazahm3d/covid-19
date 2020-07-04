e3adb8f8b97d4a789021a9d99b558533a6ca8abf
Ejaz Ahmed <ejazahmed.dev@gmail.com>
2020-07-04 13:37:27 +0500

    Commented out themeprovider for now temporarily

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import Home from "./pages";
 import { store } from "./store";
 import { Provider } from "react-redux";
-import { ThemeProvider } from "@material-ui/core";
+// import { ThemeProvider } from "@material-ui/core";
 
 function App() {
   return (
     <Provider store={store}>
-      <ThemeProvider>
-        <Home />
-      </ThemeProvider>
+      {/* <ThemeProvider> */}
+      <Home />
+      {/* </ThemeProvider> */}
     </Provider>
   );
 }
