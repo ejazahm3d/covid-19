9cdff03461f31437551e9ce5a07599cfad3cc3b2
Ejaz Ahmed <ejazahmed.dev@gmail.com>
2020-07-05 11:52:17 +0500

    Added animations on summary block

--- src/components/Blocks/Home/SummaryBlock/index.jsx
+++ src/components/Blocks/Home/SummaryBlock/index.jsx
@@ -2,19 +2,38 @@ import React from "react";
 import { Grid, Typography } from "@material-ui/core";
 import CovidSummary from "../../../CovidSummary";
 import GlobalSummaryTable from "../../../GlobalSummaryTable";
+import { motion } from "framer-motion";
 
 const SummaryBlock = () => {
   return (
     <Grid item xs={12} sm={12} md={12} lg={12} style={{ marginBottom: "2rem" }}>
-      <Typography style={{ marginBottom: "3rem" }} variant="h3">
-        Covid Summary
-      </Typography>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ ease: "easeInOut", duration: 1 }}
+      >
+        <Typography style={{ marginBottom: "3rem" }} variant="h3">
+          Covid Summary
+        </Typography>
+      </motion.div>
       <Grid container justify="space-around">
         <Grid item xs={12} sm={12} md={8}>
-          <GlobalSummaryTable />
+          <motion.div
+            initial={{ x: "-100vw", opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{ ease: "easeInOut", duration: 1 }}
+          >
+            <GlobalSummaryTable />
+          </motion.div>
         </Grid>
         <Grid item xs={12} sm={12} md={3}>
-          <CovidSummary />
+          <motion.div
+            initial={{ x: "100vw", opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{ ease: "easeInOut", duration: 1 }}
+          >
+            <CovidSummary />
+          </motion.div>
         </Grid>
       </Grid>
     </Grid>
