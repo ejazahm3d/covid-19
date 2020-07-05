ad89ca579c7e85ccec860c3a7109e263ab3e8085
Ejaz Ahmed <ejazahmed.dev@gmail.com>
2020-07-05 09:54:57 +0500

    Did initial implementation of table

--- src/components/Blocks/Home/SummaryBlock/index.jsx
+++ src/components/Blocks/Home/SummaryBlock/index.jsx
@@ -6,10 +6,10 @@ import GlobalSummaryTable from "../../../GlobalSummaryTable";
 const SummaryBlock = () => {
   return (
     <Grid container justify="center">
-      <Grid item sm={12} md={6}>
+      <Grid item xs={12} sm={12} md={6}>
         <CovidSummary />
       </Grid>
-      <Grid item sm={12} md={6}>
+      <Grid item xs={12} sm={12} md={6}>
         <GlobalSummaryTable />
       </Grid>
     </Grid>
--- src/components/GlobalSummaryTable/index.jsx
+++ src/components/GlobalSummaryTable/index.jsx
@@ -1,13 +1,131 @@
 import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { fetchGlobalSummaryData } from "../../store/actions";
+import {
+  makeStyles,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TablePagination,
+  TableRow,
+} from "@material-ui/core";
+import sortBy from "lodash.sortby";
+
+const useStyles = makeStyles({
+  root: {
+    width: "100%",
+  },
+  container: {
+    maxHeight: 440,
+  },
+});
+
+const columns = [
+  { id: "name", label: "Name", minWidth: 170 },
+  { id: "code", label: "ISO\u00a0Code", minWidth: 100 },
+  {
+    id: "confirmed",
+    label: "Total\u00a0Confirmed",
+    minWidth: 170,
+    align: "right",
+    format: (value) => value.toLocaleString("en-US"),
+  },
+  {
+    id: "recovered",
+    label: "Total\u00a0Recovered",
+    minWidth: 170,
+    align: "right",
+    format: (value) => value.toLocaleString("en-US"),
+  },
+  {
+    id: "deaths",
+    label: "Total\u00a0Deaths",
+    minWidth: 170,
+    align: "right",
+    format: (value) => value.toFixed(2),
+  },
+];
 
 const GlobalSummaryTable = () => {
+  const classes = useStyles();
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(fetchGlobalSummaryData());
   }, [dispatch]);
-  return <></>;
+
+  const { data, loading } = useSelector((state) => state.globalSummary);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
+
+  if (loading && data?.Countries?.[0]) return <div>Loading...</div>;
+  const countries = data?.Countries;
+  const sortedCountries = sortBy(countries, "TotalConfirmed").reverse();
+  const rows = sortedCountries?.map((country) => ({
+    name: country.Country,
+    code: country.CountryCode,
+    confirmed: country.TotalConfirmed,
+    deaths: country.TotalDeaths,
+    recovered: country.TotalRecovered,
+  }));
+  console.log(countries);
+  return (
+    <Paper className={classes.root}>
+      <TableContainer className={classes.container}>
+        <Table stickyHeader aria-label="sticky table">
+          <TableHead>
+            <TableRow>
+              {columns.map((column) => (
+                <TableCell key={column.id} align={column.align}>
+                  {column.label}
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows
+              ?.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((row) => {
+                return (
+                  <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+                    {columns.map((column) => {
+                      const value = row[column.id];
+                      return (
+                        <TableCell key={column.id} align={column.align}>
+                          {column.format && typeof value === "number"
+                            ? column.format(value)
+                            : value}
+                        </TableCell>
+                      );
+                    })}
+                  </TableRow>
+                );
+              })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[10, 25, 100]}
+        component="div"
+        count={rows?.length ?? 0}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onChangePage={handleChangePage}
+        onChangeRowsPerPage={handleChangeRowsPerPage}
+      />
+    </Paper>
+  );
 };
 
 export default GlobalSummaryTable;
